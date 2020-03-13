72cae0b9a6202d182c186b90551b917591ef236b
Relz <relz0071@gmail.com>
2020-03-13 07:57:07 +0300

    Добавить поиск кратчайших расстояний от начального города до остальных с помощью алгоритма Дейкстры

--- DijkstraAlgorithmViaBinaryHeap/main.cpp
+++ DijkstraAlgorithmViaBinaryHeap/main.cpp
@@ -1,8 +1,32 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
 #include "Input-Library/Input.h"
 
 using namespace std;
+
+struct Relation
+{
+	Relation(unsigned cityId, unsigned distance)
+		: cityId(cityId)
+		, distance(distance)
+	{
+
+	}
+
+	unsigned cityId;
+	unsigned distance;
+
+	bool operator <(Relation const & rhs) const
+	{
+		return distance < rhs.distance;
+	}
+};
+
+vector<unsigned> InitializeMinDistances(unsigned vertexCount, unsigned firstVertexIndex);
 
 int main()
 {
@@ -35,11 +59,22 @@ int main()
 		cerr << "Unable to read last city id from input." << endl;
 		return EXIT_FAILURE;
 	}
+
+	input.SkipLine();
 	
 	cout << cityCount  << endl
 		<< roadCount << endl
 		<< firstCityId << endl
 		<< lastCityId << endl;
+
+	vector<unsigned> minDistances = InitializeMinDistances(cityCount, firstCityId);
+
+	set<Relation> relations;
+	relations.emplace(Relation(firstCityId, 0));
+
+	unordered_set<unsigned> visitedCities;
+
+	unordered_map<unsigned, set<Relation>> cityIdsRelations;
 
 	for (unsigned i = 0; i < roadCount; ++i)
 	{
@@ -63,7 +98,44 @@ int main()
 			cerr << "Unable to read distance, line: " << (i + 1) << endl;
 			return EXIT_FAILURE;
 		}
+
+		input.SkipLine();
+
+		cityIdsRelations[sourceCityId].emplace(Relation(destinationCityId, distance));
+		cityIdsRelations[destinationCityId].emplace(Relation(sourceCityId, distance));
+	}
+
+	while (!relations.empty())
+	{
+		Relation const & relation = *relations.begin();
+
+		if (visitedCities.find(relation.cityId) != visitedCities.end())
+		{
+			relations.erase(relation);
+			continue;
+		}
+
+		set<Relation> const& cityRelations = cityIdsRelations[relation.cityId];
+		for (Relation const& cityRelation : cityRelations)
+		{
+			unsigned newDistance = relation.distance + cityRelation.distance;
+			if (newDistance < minDistances[cityRelation.cityId])
+			{
+				minDistances[cityRelation.cityId] = newDistance;
+				relations.emplace(Relation(cityRelation.cityId, newDistance));
+			}
+		}
+		visitedCities.emplace(relation.cityId);
+
+		relations.erase(relation);
 	}
 
 	return EXIT_SUCCESS;
+}
+
+vector<unsigned> InitializeMinDistances(unsigned vertexCount, unsigned firstVertexIndex)
+{
+	vector<unsigned> result(vertexCount + 1, UINT_MAX);
+	result[firstVertexIndex] = 0;
+	return result;
 }
